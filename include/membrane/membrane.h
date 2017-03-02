dcf201254913ef9c8afbd93e6bf26cf629b961bd
Mateusz Front <mateusz.front@swmansion.com>
2017-03-02 12:48:22 +0100

    changed membrane_sample_format consts to macros

--- include/membrane/membrane.h
+++ include/membrane/membrane.h
@@ -27,15 +27,15 @@
   MEMBRANE_UTIL_PARSE_ARG(no, var, enif_get_resource, handle_resource_type, pointer_to_var)
 
 // format encoding constants
-const uint32_t MEMBRANE_SAMPLE_FORMAT_TYPE = 0b11 << 30;
-const uint32_t MEMBRANE_SAMPLE_FORMAT_TYPE_SIGN = 0b1 << 30;
-const uint32_t MEMBRANE_SAMPLE_FORMAT_ENDIANITY = 0b1 << 29;
-const uint32_t MEMBRANE_SAMPLE_FORMAT_SIZE = (0b1 << 8) - 1;
-const uint32_t MEMBRANE_SAMPLE_FORMAT_TYPE_U = 0b00 << 30;
-const uint32_t MEMBRANE_SAMPLE_FORMAT_TYPE_S = 0b01 << 30;
-const uint32_t MEMBRANE_SAMPLE_FORMAT_TYPE_F = 0b11 << 30;
-const uint32_t MEMBRANE_SAMPLE_FORMAT_ENDIANITY_LE = 0b0 << 29;
-const uint32_t MEMBRANE_SAMPLE_FORMAT_ENDIANITY_BE = 0b1 << 29;
+#define MEMBRANE_SAMPLE_FORMAT_TYPE ((uint32_t)(0b11 << 30))
+#define MEMBRANE_SAMPLE_FORMAT_TYPE_SIGN ((uint32_t)(0b1 << 30))
+#define MEMBRANE_SAMPLE_FORMAT_ENDIANITY ((uint32_t)(0b1 << 29))
+#define MEMBRANE_SAMPLE_FORMAT_SIZE ((uint32_t)((0b1 << 8) - 1))
+#define MEMBRANE_SAMPLE_FORMAT_TYPE_U ((uint32_t)(0b00 << 30))
+#define MEMBRANE_SAMPLE_FORMAT_TYPE_S ((uint32_t)(0b01 << 30))
+#define MEMBRANE_SAMPLE_FORMAT_TYPE_F ((uint32_t)(0b11 << 30))
+#define MEMBRANE_SAMPLE_FORMAT_ENDIANITY_LE ((uint32_t)(0b0 << 29))
+#define MEMBRANE_SAMPLE_FORMAT_ENDIANITY_BE ((uint32_t)(0b1 << 29))
 
 
 
