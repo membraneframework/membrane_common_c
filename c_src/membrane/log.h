1de0a33fae0f9950ef41f083c43d353c82e25a5f
Mateusz Front <mateusz.front@swmansion.com>
2018-02-02 22:05:29 +0100

    logs: passing log_tag instead of default_log_tag to membrane_log_format

--- c_src/membrane/log.h
+++ c_src/membrane/log.h
@@ -22,15 +22,15 @@
 #define MEMBRANE_LOG_TAG MEMBRANE_DEFAULT_TAG
 #endif
 
-#define MEMBRANE_DEBUG(env, message, ...) membrane_log_format(env, MEMBRANE_LOG_LEVEL_DEBUG, "[" MEMBRANE_LOG_TAG "] " message "\r\n", ##__VA_ARGS__ )
-#define MEMBRANE_INFO(env, message, ...) membrane_log_format(env, MEMBRANE_LOG_LEVEL_INFO, "[" MEMBRANE_LOG_TAG "] " message "\r\n", ##__VA_ARGS__ )
-#define MEMBRANE_WARN(env, message, ...) membrane_log_format(env, MEMBRANE_LOG_LEVEL_WARN, "[" MEMBRANE_LOG_TAG "] " message "\r\n", ##__VA_ARGS__ )
-#define MEMBRANE_THREADED_DEBUG(message, ...) membrane_log_format(NULL, MEMBRANE_LOG_LEVEL_DEBUG, "[" MEMBRANE_LOG_TAG "] " message "\r\n", ##__VA_ARGS__ )
-#define MEMBRANE_THREADED_INFO(message, ...) membrane_log_format(NULL, MEMBRANE_LOG_LEVEL_INFO, "[" MEMBRANE_LOG_TAG "] " message "\r\n", ##__VA_ARGS__ )
-#define MEMBRANE_THREADED_WARN(message, ...) membrane_log_format(NULL, MEMBRANE_LOG_LEVEL_WARN, "[" MEMBRANE_LOG_TAG "] " message "\r\n", ##__VA_ARGS__ )
+#define MEMBRANE_DEBUG(env, message, ...) membrane_log_format(env, MEMBRANE_LOG_LEVEL_DEBUG, MEMBRANE_LOG_TAG, message "\r\n", ##__VA_ARGS__ )
+#define MEMBRANE_INFO(env, message, ...) membrane_log_format(env, MEMBRANE_LOG_LEVEL_INFO, MEMBRANE_LOG_TAG, message "\r\n", ##__VA_ARGS__ )
+#define MEMBRANE_WARN(env, message, ...) membrane_log_format(env, MEMBRANE_LOG_LEVEL_WARN, MEMBRANE_LOG_TAG, message "\r\n", ##__VA_ARGS__ )
+#define MEMBRANE_THREADED_DEBUG(message, ...) membrane_log_format(NULL, MEMBRANE_LOG_LEVEL_DEBUG, MEMBRANE_LOG_TAG, message "\r\n", ##__VA_ARGS__ )
+#define MEMBRANE_THREADED_INFO(message, ...) membrane_log_format(NULL, MEMBRANE_LOG_LEVEL_INFO, MEMBRANE_LOG_TAG, message "\r\n", ##__VA_ARGS__ )
+#define MEMBRANE_THREADED_WARN(message, ...) membrane_log_format(NULL, MEMBRANE_LOG_LEVEL_WARN, MEMBRANE_LOG_TAG, message "\r\n", ##__VA_ARGS__ )
 
 int membrane_log(ErlNifEnv *env, ErlNifEnv *msg_env, int level, ERL_NIF_TERM message, ERL_NIF_TERM tags);
-int membrane_log_format(ErlNifEnv *env, int level, const char *format, ...);
+int membrane_log_format(ErlNifEnv *env, int level, const char *log_tag, const char *format, ...);
 ERL_NIF_TERM membrane_log_level_to_term(ErlNifEnv *env, int level);
 
 #endif
