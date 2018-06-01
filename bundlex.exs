560b6a61164831227e32471fa7476e8463c1c77f
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2018-06-01 17:02:40 +0200

    Clear another PortAudio leftover

--- bundlex.exs
+++ bundlex.exs
@@ -17,7 +17,7 @@ defmodule Membrane.Common.C.BundlexProject do
       membrane_ringbuffer: [
         export_only?: Mix.env() != :test,
         src_base: "membrane_ringbuffer",
-        sources: ["ringbuffer.c", "portaudio/pa_ringbuffer.c"]
+        sources: ["ringbuffer.c"]
       ]
     ]
   end
